8e5269cd064d4f8839989a2f342a24e404299433
NandithaNagulapally <nanditha.nagulapally9@gmail.com>
2021-07-06 16:21:07 +0530

    Trail 3

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,12 @@
 def primary():
-     print("Keep it logically awesome.")
-     print("Practicing")
-     print("second line")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  print("Keep it logically awesome.")
+  print("Practicing")
+  print("second line")
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  print(quotes)
+  print(quotes[13])
 
 if __name__== "__main__":
   primary()
